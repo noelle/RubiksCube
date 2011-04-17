c9462dec8d63eb53e663279ddacc46db94b96b75
noelle <nrosenbe@hsz-t.ch>
2011-04-17 19:55:37 +0200

    - Cubie model class implemented - Surface class deleted

--- RubiksCube/Model/Cubie.cs
+++ RubiksCube/Model/Cubie.cs
@@ -5,7 +5,77 @@ using System.Text;
 
 namespace RubiksCube.Model
 {
-    class Cubie
+    enum CubieType
     {
+        Corner,
+        Center,
+        Edge
+    }
+
+    public abstract class Cubie
+    {
+        private CubieType type;
+
+        /// <summary>
+        /// Koordinate des Cubies in der Koordinaten-Achse des Cubes
+        /// </summary>
+        private int posX;
+        public int PosX
+        {
+            get { return posX; }
+            set { posX = value; }
+        }
+
+        private int posY;
+        public int PosY
+        {
+            get { return posY; }
+            set { posY = value; }
+        }
+
+        private int posZ;
+        public int PosZ
+        {
+            get { return posZ; }
+            set { posZ = value; }
+        }
+
+        /// <summary>
+        /// Farben der Cubie Koordinaten-Flächen
+        /// </summary>
+        private string colX;
+        public string ColX
+        {
+            get { return colX; }
+            set { colX = value; }
+        }
+
+        private string colY;
+        public string ColY
+        {
+            get { return colY; }
+            set { colY = value; }
+        }
+
+        private string colZ;
+        public string ColZ
+        {
+            get { return colZ; }
+            set { colZ = value; }
+        }
+        
+
+        public Cubie(CubieType type, int xPos, int yPos, int zPos, string xCol, string yCol, string zCol)
+        {
+            this.type = type;
+
+            this.posX = xPos;
+            this.posY = yPos;
+            this.posZ = zPos;
+
+            this.colX = xCol;
+            this.colY = yCol;
+            this.colZ = zCol;            
+        }
     }
 }
