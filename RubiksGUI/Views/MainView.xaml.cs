2dede9f9bf789362e2e4d0dafaf3b7bfdf16ec66
Selim Oeksuez <soeksuez@hsz-t.ch>
2011-05-10 14:59:07 +0200

    started with GUI designing

--- RubiksGUI/Views/MainView.xaml.cs
+++ RubiksGUI/Views/MainView.xaml.cs
@@ -12,10 +12,24 @@ namespace RubiksGUI
 {
 	public partial class MainView : UserControl
 	{
+        private System.Windows.Media.Brush activeColor;
+
 		public MainView()
 		{
 			// Required to initialize variables
 			InitializeComponent();
 		}
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Shapes.Rectangle obj = (System.Windows.Shapes.Rectangle)sender;
+            obj.Fill = activeColor;
+        }
+
+        private void Color_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Shapes.Rectangle obj = (System.Windows.Shapes.Rectangle)sender;
+            this.activeColor = obj.Fill;
+        }
 	}
 }
